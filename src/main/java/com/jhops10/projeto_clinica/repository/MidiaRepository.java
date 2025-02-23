b983492f3b254c1a206f7c07b7829847526df9cf
João Paulo <joaowais@gmail.com>
2025-02-22 22:06:53 -0300

    Adiciona MidiaRepository

---
+++ src/main/java/com/jhops10/projeto_clinica/repository/MidiaRepository.java
@@ -0,0 +1,7 @@
+package com.jhops10.projeto_clinica.repository;
+
+import com.jhops10.projeto_clinica.model.Midia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MidiaRepository extends JpaRepository<Midia, Integer> {
+}
